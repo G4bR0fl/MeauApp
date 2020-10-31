36c61056e2cfd03535f9bddde692abb8da9cf861
santosdanilo <dandesenvolvimento@gmail.com>
2020-10-31 09:58:49 -0300

    .

--- src/routes/ProfileStack.tsx
+++ src/routes/ProfileStack.tsx
@@ -48,7 +48,6 @@ export default function ProfileStack({ navigation }) {
         name={Home.name}
         component={Home}
         options={{
-          title: 'Home',
           headerStyle: {
             backgroundColor: 'white',
           },
