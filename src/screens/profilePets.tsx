f8ec84b7452d86654beb28571649ad794b2eb5b3
santosdanilo <dandesenvolvimento@gmail.com>
2020-11-16 08:25:05 -0300

    Adicionada listagem de pets

--- src/screens/profilePets.tsx
+++ src/screens/profilePets.tsx
@@ -1,7 +1,7 @@
-import { Api } from '@backend/firebase/api';
-import DogCardProfile from '@mobile/components/DogCardProfile';
 import React, { useEffect, useState } from 'react';
 import { ScrollView } from 'react-native';
+import { Api } from '../../backend/firebase/api';
+import DogCardProfile from '../components/DogCardProfile';
 import { Container } from '../components/layout';
 import PetDetail from './petAdopt';
 
@@ -9,7 +9,7 @@ export default function ProfilePets({ navigation }) {
   const [list, setList] = useState<any[]>([]);
 
   useEffect(() => {
-    Api.Database.Pet.listAdoption().then(res =>
+    Api.Database.Profile.listOwnedPets().then(res =>
       setList(res.map(doc => [doc.data(), doc]))
     );
   }, []);
