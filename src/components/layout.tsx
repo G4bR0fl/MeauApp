af67e147e3508f81aed3e087a1291ec585f06667
santosdanilo <dandesenvolvimento@gmail.com>
2020-11-17 18:56:13 -0300

    Adicionando campo de intenção

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,6 +2,6 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   min-height: 100%;
-  padding: 0% 5% 10% 5%;
+  padding: 10px 24px 10% 24px;
   width: 100%;
 `;
